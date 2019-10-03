7b36fca4b047def633eac076bb5d7f714a3ffe2c
LilianLem <54894780+LilianLem@users.noreply.github.com>
2019-10-03 07:57:27 +0200

    Revert "Revert "Mise en place de workbox (PWA)""
    This reverts commit 2df18006c1b9c5f2d4442aad0c2e82a6cb111fe5.

---
+++ src-sw.js
@@ -0,0 +1,19 @@
+// Importation de workbox depuis un CDN
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+// Ajout de code personnalisé
+console.log('this is my custom service worker');
+
+// Routing
+workbox.routing.registerRoute(
+  new RegExp('/css|fonts|img|js/'),
+  workbox.strategies.cacheFirst()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/index.html/'),
+  new workbox.strategies.NetworkFirst()
+);
+
+//Ajout d'un placeholder 
+workbox.precaching.precacheAndRoute([]);
