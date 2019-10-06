7a01ccfe6956551f2c1b8f47ab8df62541eef76f
LilianLem <54894780+LilianLem@users.noreply.github.com>
2019-10-06 23:14:05 +0200

    Suite de la mise en place de webpack
    Installation de MiniCssExtractPlugin, HtmlWebpackPlugin, Babel, Autoprefixer et Babel Polyfill.
    HtmlWebpackPlugin fonctionne mais Polyfill n'a pas été intégré dans package.json, le plugin Mini CSS Extract n'est pas fonctionnel alors que j'ai suivi le tuto Hackernoon et une erreur s'affiche dans la console au chargement d'index.html (regeneratorRuntime is not defined).

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
 const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let config = {
   entry: "./src/scripts.js",
+  resolve: {extensions: ['.js', '.ts']},
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "./scripts.js"
   },
+  plugins: [
+      new HtmlWebpackPlugin({
+          title: 'Infos Vélocité en temps réel - Vélo Mulhouse Live',
+          template: './src/index.html',
+          inject: false,
+          minify: {
+              removeComments: true,
+              collapseWhitespace: false
+          }
+      }),
+      new MiniCssExtractPlugin({
+          filename: 'styles.css'
+      }),
+      require('autoprefixer')
+  ],
   module: {
     rules: [{
       test: /\.scss$/,
-      loader: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+      loader: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+    },
+    {
+      test: [/.js$/],
+      exclude: /(node_modules)/,
+      use: {
+          loader: 'babel-loader',
+          options: {
+              presets: [
+                  '@babel/preset-env'
+              ]
+          }
+      }
     }]
   }
 }
